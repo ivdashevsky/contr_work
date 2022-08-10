da3b8863c036128e7293fe3289760dc0226d3f79
Ilya <iii@ya.ru>
2022-08-10 10:34:34 +0500

    создан запрос

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿string [] Array = /*{"Russia","Denmark","Kazan"};*/ /*{"1234","1567","-2","computer "};*/ {"Hello", "2", "World", ":-)"};
+int count = 0;
+int final_count = 0;
+
+
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].ToString().Length <= 3)
+    {
+        final_count++;
+    }
+
+}
+
+string [] result = new string [final_count];
+
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].ToString().Length <= 3)
+    {
+        result[count] = Array[i];
+        count++;
+    }
+}
+
+Console.WriteLine(String.Join(" ",result));
